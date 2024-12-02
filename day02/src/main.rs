aecd946067bbaa9640a2f9e18340540481aa79bc
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-12-02 22:34:13 +0100

    chore: Add `.txt` suffix to input files

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -3,7 +3,7 @@ use utils::input;
 const DAY: u32 = 2;
 
 fn main() {
-    let input = input::read_file(&format!("day{:02}", DAY));
+    let input = input::read_file(&format!("day{:02}.txt", DAY));
     println!("exercise 1: {}", exercise1(&input));
     println!("exercise 2: {}", exercise2(&input));
 }
@@ -64,14 +64,14 @@ mod test {
 
     #[test]
     fn test_ex1() {
-        let input = input::read_file(&format!("day{:02}_example", DAY));
+        let input = input::read_file(&format!("day{:02}_example.txt", DAY));
         let res = exercise1(&input);
         assert_eq!(res, 2);
     }
 
     #[test]
     fn test_ex2() {
-        let input = input::read_file(&format!("day{:02}_example", DAY));
+        let input = input::read_file(&format!("day{:02}_example.txt", DAY));
         let res = exercise2(&input);
         assert_eq!(res, 4);
     }
