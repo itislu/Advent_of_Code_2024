7968a0eb3faab5d78a5366c9b78c31fd7d8c0329
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-12-02 22:31:25 +0100

    [02] feat: Solve exercise 2 and reuse code

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -5,7 +5,7 @@ const DAY: u32 = 2;
 fn main() {
     let input = input::read_file(&format!("day{:02}", DAY));
     println!("exercise 1: {}", exercise1(&input));
-    // println!("exercise 2: {}", exercise2(&input));
+    println!("exercise 2: {}", exercise2(&input));
 }
 
 fn exercise1(input: &String) -> usize {
@@ -15,21 +15,47 @@ fn exercise1(input: &String) -> usize {
             .split_ascii_whitespace()
             .map(|n| n.parse::<i32>().unwrap())
             .collect();
-        let mut is_close = true;
-        for window in numbers.windows(2) {
-            if (window[0] - window[1] < 0) != (numbers[0] - numbers[1] < 0)
-                || window[0].abs_diff(window[1]) < 1
-                || window[0].abs_diff(window[1]) > 3
-            {
-                is_close = false;
-                break;
-            }
-        }
-        if is_close {
+
+        if is_close(&numbers) {
             res += 1;
         }
     }
     res
+}
+
+fn exercise2(input: &String) -> usize {
+    let mut res: usize = 0;
+    for line in input.lines() {
+        let numbers: Vec<i32> = line
+            .split_ascii_whitespace()
+            .map(|n| n.parse::<i32>().unwrap())
+            .collect();
+
+        for skip in 0..numbers.len() {
+            let one_less: Vec<i32> = numbers
+                .iter()
+                .enumerate()
+                .filter_map(|(i, &n)| if i != skip { Some(n) } else { None })
+                .collect();
+            if is_close(&one_less) {
+                res += 1;
+                break;
+            }
+        }
+    }
+    res
+}
+
+fn is_close(numbers: &Vec<i32>) -> bool {
+    for window in numbers.windows(2) {
+        if (window[0] - window[1] < 0) != (numbers[0] - numbers[1] < 0)
+            || window[0].abs_diff(window[1]) < 1
+            || window[0].abs_diff(window[1]) > 3
+        {
+            return false;
+        }
+    }
+    true
 }
 
 #[cfg(test)]
@@ -43,10 +69,10 @@ mod test {
         assert_eq!(res, 2);
     }
 
-    // #[test]
-    // fn test_ex2() {
-    //     let input = input::read_file(&format!("day{:02}_example", DAY));
-    //     let res = exercise2(&input);
-    //     assert_eq!(res, 31);
-    // }
+    #[test]
+    fn test_ex2() {
+        let input = input::read_file(&format!("day{:02}_example", DAY));
+        let res = exercise2(&input);
+        assert_eq!(res, 4);
+    }
 }
