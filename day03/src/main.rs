0e9c06ea30f2d17567b510441e1617a5b858c24e
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-12-03 11:57:10 +0100

    [03] feat: Solve exercise 2 and reuse code

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -4,18 +4,39 @@ use utils::input;
 fn main() {
     let input = input::read_input();
     println!("exercise 1: {}", exercise1(&input));
+    println!("exercise 2: {}", exercise2(&input));
 }
 
 fn exercise1(input: &String) -> u64 {
+    calculate(input)
+}
+
+fn exercise2(mut input: &str) -> u64 {
+    let mut res: u64 = 0;
+    let mut is_do: bool = true;
+    let mut end: usize;
+
+    while !input.is_empty() {
+        if is_do {
+            end = input.find("don't()").unwrap_or(input.len());
+            res += calculate(&input[..end]);
+        } else {
+            end = input.find("do()").unwrap_or(input.len());
+        }
+        is_do = !is_do;
+        input = &input[end..];
+    }
+    res
+}
+
+fn calculate(substr: &str) -> u64 {
     let re = Regex::new(r"mul\((?<n1>[+-]?\d+),(?<n2>[+-]?\d+)\)").unwrap();
-    let res: u64 = re
-        .captures_iter(input)
+    re.captures_iter(substr)
         .map(|caps| {
             caps.name("n1").unwrap().as_str().parse::<u64>().unwrap()
                 * caps.name("n2").unwrap().as_str().parse::<u64>().unwrap()
         })
-        .sum();
-    res
+        .sum()
 }
 
 #[cfg(test)]
@@ -27,5 +48,12 @@ mod test {
         let input = input::read_example();
         let res = exercise1(&input);
         assert_eq!(res, 161);
+    }
+
+    #[test]
+    fn test_ex2() {
+        let input = input::read_example();
+        let res = exercise2(&input);
+        assert_eq!(res, 48);
     }
 }
