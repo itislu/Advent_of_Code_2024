2f1ec5c13f9e4f914f4c8d2e235f420a8bfd310d
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-12-05 18:12:07 +0100

    [05] feat: Solve exercise 2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,8 +1,10 @@
+use std::cmp::Ordering;
 use utils::input;
 
 fn main() {
     let input = input::read_input();
     println!("exercise 1: {}", exercise1(&input));
+    println!("exercise 2: {}", exercise2(&input));
 }
 
 fn exercise1(input: &String) -> usize {
@@ -20,8 +22,37 @@ fn exercise1(input: &String) -> usize {
         .sum()
 }
 
+fn exercise2(input: &String) -> usize {
+    let rules: Vec<(u32, u32)> = parse_rules(input);
+    let mut updates = parse_updates(input);
+    let mut bad_updates: Vec<&mut Vec<u32>> = updates
+        .iter_mut()
+        .filter(|update| {
+            update
+                .windows(2)
+                .any(|window| !is_correct_pair(window[0], window[1], &rules))
+        })
+        .collect();
+
+    bad_updates
+        .iter_mut()
+        .for_each(|update| update.sort_by(|&a, &b| cmp_pair(a, b, &rules)));
+    bad_updates
+        .iter()
+        .map(|update| update[update.len() / 2] as usize)
+        .sum()
+}
+
 fn is_correct_pair(n1: u32, n2: u32, rules: &Vec<(u32, u32)>) -> bool {
     rules.iter().any(|&rule| rule.0 == n1 && rule.1 == n2)
+}
+
+fn cmp_pair(n1: u32, n2: u32, rules: &Vec<(u32, u32)>) -> Ordering {
+    if is_correct_pair(n1, n2, rules) {
+        Ordering::Less
+    } else {
+        Ordering::Greater
+    }
 }
 
 fn parse_rules(input: &String) -> Vec<(u32, u32)> {
@@ -62,5 +93,12 @@ mod test {
         let input = input::read_example();
         let res = exercise1(&input);
         assert_eq!(res, 143);
+    }
+
+    #[test]
+    fn test_ex2() {
+        let input = input::read_example();
+        let res = exercise2(&input);
+        assert_eq!(res, 123);
     }
 }
