cf23d0b94bcbe868520c10750a6ffc5ab797f891
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-12-07 23:28:40 +0100

    [07] style: Rename `operator_map` to `punch_card`

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -12,32 +12,32 @@ fn exercise1(input: &String) -> i64 {
     for line in input.lines() {
         let target = parse_target(line);
         let numbers = parse_numbers(line);
-        let mut operator_map: usize = 2_usize.pow(numbers.len() as u32 - 1);
+        let mut punch_card: usize = 2_usize.pow(numbers.len() as u32 - 1);
 
         loop {
-            let operator = Operator::new(operator_map, &numbers);
+            let operator = Operator::new(punch_card, &numbers);
             if operator.calculate() == target {
                 res += target;
                 break;
             }
-            if operator_map == 0 {
+            if punch_card == 0 {
                 break;
             }
-            operator_map -= 1;
+            punch_card -= 1;
         }
     }
     res
 }
 
 struct Operator<'a> {
-    operator_map: usize,
+    punch_card: usize,
     numbers: &'a Vec<i64>,
 }
 
 impl<'a> Operator<'a> {
-    fn new(operator_map: usize, numbers: &'a Vec<i64>) -> Self {
+    fn new(punch_card: usize, numbers: &'a Vec<i64>) -> Self {
         Operator {
-            operator_map,
+            punch_card,
             numbers,
         }
     }
@@ -48,7 +48,7 @@ impl<'a> Operator<'a> {
         }
         let mut res: i64 = self.numbers[0];
         for (i, number) in self.numbers.iter().skip(1).enumerate() {
-            match self.operator_map.saturating_sub(i) & 1 {
+            match self.punch_card.saturating_sub(i) & 1 {
                 0 => res += number,
                 1 => res *= number,
                 _ => panic!(),
