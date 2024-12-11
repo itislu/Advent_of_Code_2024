da8e24d88eeda261171197cbb2f970caf5216d79
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-12-11 19:59:21 +0100

    [11] improve: Track cache stats

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use utils::input;
 
 fn main() {
@@ -21,12 +21,14 @@ fn exercise2(input: &str) -> usize {
     let mut res: usize = 0;
     let stones: Vec<usize> = parse_stones(input);
     let mut cache: HashMap<(usize, usize), usize> = HashMap::new();
+    let mut tracker = Tracker::new();
 
     for stone in stones {
-        let tmp = split_stone_n_times_cached(stone, 75, &mut cache);
+        let tmp = split_stone_n_times_cached(stone, 75, &mut cache, &mut tracker);
         cache.insert((stone, 75), tmp);
         res += tmp;
     }
+    println!("{}", tracker);
     res
 }
 
@@ -50,24 +52,31 @@ fn split_stone_n_times_cached(
     mut stone: usize,
     mut n: usize,
     cache: &mut HashMap<(usize, usize), usize>,
+    tracker: &mut Tracker,
 ) -> usize {
     if let Some(cached_result) = cache.get(&(stone, n)) {
+        tracker.cache_hit(*cached_result, stone, n);
         return *cached_result;
+    } else {
+        tracker.cache_miss();
     }
     let mut res: usize = 1;
     while n > 0 {
         if stone == 0 {
             stone = 1;
         } else if let Some((left, right)) = split_if_even_digits(stone) {
-            let res_left = split_stone_n_times_cached(left, n - 1, cache);
+            tracker.unique_stones.insert(left);
+            tracker.unique_stones.insert(right);
+            let res_left = split_stone_n_times_cached(left, n - 1, cache, tracker);
             cache.insert((left, n - 1), res_left);
-            let res_right = split_stone_n_times_cached(right, n - 1, cache);
+            let res_right = split_stone_n_times_cached(right, n - 1, cache, tracker);
             cache.insert((right, n - 1), res_right);
             res = res_left + res_right;
             break;
         } else {
             stone *= 2024;
         }
+        tracker.unique_stones.insert(stone);
         n -= 1;
     }
     res
@@ -88,6 +97,51 @@ fn parse_stones(input: &str) -> Vec<usize> {
         .split_whitespace()
         .map(|n| n.parse().unwrap())
         .collect()
+}
+
+struct Tracker {
+    cache_hits: usize,
+    cache_misses: usize,
+    saved_iterations_total: usize,
+    unique_stones: HashSet<usize>,
+}
+
+impl Tracker {
+    fn new() -> Self {
+        Tracker {
+            cache_hits: 0,
+            cache_misses: 0,
+            saved_iterations_total: 0,
+            unique_stones: HashSet::new(),
+        }
+    }
+
+    fn cache_hit(&mut self, cached_result: usize, stone: usize, iter_left: usize) {
+        self.cache_hits += 1;
+        self.saved_iterations_total += iter_left;
+        println!(
+            "cached: {}, for stone value: {}, saved iterations: {}, saved iterations total: {}, cache hits: {}, unique stones: {}",
+            cached_result, stone, iter_left, self.saved_iterations_total, self.cache_hits, self.unique_stones.len()
+        );
+    }
+
+    fn cache_miss(&mut self) {
+        self.cache_misses += 1;
+        println!("cache misses: {}", self.cache_misses);
+    }
+}
+
+impl std::fmt::Display for Tracker {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(
+            f,
+            "TRACKER:\ncache hits: {}\ncache misses: {}\nsaved iterations total: {}\nunique stones: {}",
+            self.cache_hits,
+            self.cache_misses,
+            self.saved_iterations_total,
+            self.unique_stones.len()
+        )
+    }
 }
 
 #[cfg(test)]
