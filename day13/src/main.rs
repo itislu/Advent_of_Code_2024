8387270339e1626765c7350effd8ec7e74f7a830
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-12-13 11:08:20 +0100

    [13] feat: Solve exercise 2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,18 +1,20 @@
-use std::ops::{Add, Mul};
 use itertools::Itertools;
+use std::ops::{Add, Mul};
 use utils::input;
 
 fn main() {
     let input = input::read_input();
     println!("exercise 1: {}", exercise1(&input));
+    println!("exercise 2: {}", exercise2(&input));
 }
 
-const COST_A: usize = 3;
-const COST_B: usize = 1;
-const MAX_PRESSES: usize = 100;
+const COST_A: i64 = 3;
+const COST_B: i64 = 1;
+const MAX_PRESSES: i64 = 100;
+const GREAT_DISTANCE: i64 = 10000000000000;
 
-fn exercise1(input: &str) -> usize {
-    let mut res: usize = 0;
+fn exercise1(input: &str) -> i64 {
+    let mut res: i64 = 0;
     for (button_a, button_b, prize) in parse_input(input) {
         if let Some(min_cost) = try_all_combinations(button_a, button_b, prize) {
             res += min_cost;
@@ -21,8 +23,47 @@ fn exercise1(input: &str) -> usize {
     res
 }
 
-fn try_all_combinations(button_a: Button, button_b: Button, prize: Position) -> Option<usize> {
-    let mut min_cost: Option<usize> = None;
+fn exercise2(input: &str) -> i64 {
+    let mut res: i64 = 0;
+    for (mut button_a, mut button_b, prize) in parse_input(input) {
+        if move_to_prize(&mut button_a, &mut button_b, prize + GREAT_DISTANCE) {
+            res += button_a.get_cost() + button_b.get_cost();
+        }
+    }
+    res
+}
+
+fn move_to_prize(button_a: &mut Button, button_b: &mut Button, prize: Position) -> bool {
+    let ax = button_a.movement.x;
+    let ay = button_a.movement.y;
+    let bx = button_b.movement.x;
+    let by = button_b.movement.y;
+    let px = prize.x;
+    let py = prize.y;
+
+    let a = divide_if_whole(px * by - py * bx, ax * by - ay * bx);
+    if a.is_none() {
+        return false;
+    }
+    let b = divide_if_whole(px - ax * a.unwrap(), bx);
+    if b.is_none() {
+        return false;
+    }
+    button_a.presses = a.unwrap();
+    button_b.presses = b.unwrap();
+    true
+}
+
+fn divide_if_whole(a: i64, b: i64) -> Option<i64> {
+    if a % b == 0 {
+        Some(a / b)
+    } else {
+        None
+    }
+}
+
+fn try_all_combinations(button_a: Button, button_b: Button, prize: Position) -> Option<i64> {
+    let mut min_cost: Option<i64> = None;
 
     for combination in (0..=MAX_PRESSES).permutations(2) {
         if let Some(cost) = calc_cost(
@@ -48,7 +89,7 @@ fn try_all_combinations(button_a: Button, button_b: Button, prize: Position) -> 
     min_cost
 }
 
-fn calc_cost(button_a: Button, button_b: Button, prize: Position) -> Option<usize> {
+fn calc_cost(button_a: Button, button_b: Button, prize: Position) -> Option<i64> {
     if button_a.apply() + button_b.apply() == prize {
         Some(button_a.get_cost() + button_b.get_cost())
     } else {
@@ -58,12 +99,12 @@ fn calc_cost(button_a: Button, button_b: Button, prize: Position) -> Option<usiz
 
 struct Button {
     movement: Position,
-    presses: usize,
-    cost: usize,
+    presses: i64,
+    cost: i64,
 }
 
 impl Button {
-    fn new(x: usize, y: usize, presses: usize, cost: usize) -> Self {
+    fn new(x: i64, y: i64, presses: i64, cost: i64) -> Self {
         Button {
             movement: Position::new(x, y),
             presses,
@@ -75,19 +116,19 @@ impl Button {
         self.movement * self.presses
     }
 
-    fn get_cost(&self) -> usize {
+    fn get_cost(&self) -> i64 {
         self.presses * self.cost
     }
 }
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 struct Position {
-    x: usize,
-    y: usize,
+    x: i64,
+    y: i64,
 }
 
 impl Position {
-    fn new(x: usize, y: usize) -> Self {
+    fn new(x: i64, y: i64) -> Self {
         Position { x, y }
     }
 }
@@ -103,10 +144,21 @@ impl Add for Position {
     }
 }
 
-impl Mul<usize> for Position {
+impl Add<i64> for Position {
     type Output = Position;
 
-    fn mul(self, rhs: usize) -> Self::Output {
+    fn add(self, rhs: i64) -> Self::Output {
+        Self {
+            x: self.x + rhs,
+            y: self.y + rhs,
+        }
+    }
+}
+
+impl Mul<i64> for Position {
+    type Output = Position;
+
+    fn mul(self, rhs: i64) -> Self::Output {
         Self {
             x: self.x * rhs,
             y: self.y * rhs,
@@ -116,7 +168,7 @@ impl Mul<usize> for Position {
 
 fn parse_input(input: &str) -> impl Iterator<Item = (Button, Button, Position)> + '_ {
     input.split("\n\n").map(|block| {
-        let numbers: Vec<Vec<usize>> = block.lines().map(|line| parse_numbers(line)).collect();
+        let numbers: Vec<Vec<i64>> = block.lines().map(|line| parse_numbers(line)).collect();
         (
             Button::new(numbers[0][0], numbers[0][1], 0, COST_A),
             Button::new(numbers[1][0], numbers[1][1], 0, COST_B),
@@ -125,8 +177,8 @@ fn parse_input(input: &str) -> impl Iterator<Item = (Button, Button, Position)> 
     })
 }
 
-fn parse_numbers(s: &str) -> Vec<usize> {
-    let mut numbers: Vec<usize> = Vec::new();
+fn parse_numbers(s: &str) -> Vec<i64> {
+    let mut numbers: Vec<i64> = Vec::new();
     let mut current_number = String::new();
 
     for c in s.chars() {
@@ -152,5 +204,12 @@ mod test {
         let input = input::read_example();
         let res = exercise1(&input);
         assert_eq!(res, 480);
+    }
+
+    #[test]
+    fn test_ex2() {
+        let input = input::read_example();
+        let res = exercise2(&input);
+        println!("{}", res);
     }
 }
