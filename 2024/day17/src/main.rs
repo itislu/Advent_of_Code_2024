3c1ab3665e8d966d8a437e68095933c9a8d1f07b
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-12-17 15:49:47 +0100

    [17] feat: Solve exercise 2 (with luck)

--- 2024/day17/src/main.rs
+++ 2024/day17/src/main.rs
@@ -1,14 +1,35 @@
+use itertools::Itertools;
 use utils::input;
 
 fn main() {
     let input = input::read_input();
     println!("exercise 1: {}", exercise1(&input));
+    println!("exercise 2: {}", exercise2(&input));
 }
 
 fn exercise1(input: &str) -> String {
     let mut computer = Computer::new(input);
     computer.run();
     computer.output()
+}
+
+fn exercise2(input: &str) -> usize {
+    let mut computer = Computer::new(input);
+    let mut reg_a = 202992820169258;
+    loop {
+        computer.reset(reg_a);
+        if computer.run_until_copy() {
+            println!(
+                "trying {} -> {} (len: {})",
+                reg_a,
+                computer.output(),
+                computer.out.len()
+            );
+            // break;
+        }
+        reg_a -= 8;
+    }
+    reg_a
 }
 
 struct Computer {
@@ -18,6 +39,7 @@ struct Computer {
     instr: Vec<usize>,
     ptr: usize,
     out: Vec<char>,
+    out_cnt: usize,
 }
 
 impl Computer {
@@ -40,17 +62,17 @@ impl Computer {
             instr,
             ptr: 0,
             out: Vec::new(),
+            out_cnt: 0,
         }
     }
 
-    fn combo(&self, operand: usize) -> usize {
-        match operand {
-            0..=3 => operand,
-            4 => self.reg_a,
-            5 => self.reg_b,
-            6 => self.reg_c,
-            _ => panic!("Invalid combo operand found!"),
-        }
+    fn reset(&mut self, reg_a: usize) {
+        self.reg_a = reg_a;
+        self.reg_b = 0;
+        self.reg_c = 0;
+        self.ptr = 0;
+        self.out.clear();
+        self.out_cnt = 0;
     }
 
     fn run(&mut self) {
@@ -71,6 +93,52 @@ impl Computer {
             if self.ptr == prev_ptr {
                 self.ptr += 2;
             }
+        }
+    }
+
+    fn run_until_copy(&mut self) -> bool {
+        while self.ptr < self.instr.len() {
+            if !self.is_out_still_same() {
+                return false;
+            }
+
+            let prev_ptr = self.ptr;
+            let operand = self.instr[self.ptr + 1];
+            match self.instr[self.ptr] {
+                0 => self.adv(operand),
+                1 => self.bxl(operand),
+                2 => self.bst(operand),
+                3 => self.jnz(operand),
+                4 => self.bxc(operand),
+                5 => self.out(operand),
+                6 => self.bdv(operand),
+                7 => self.cdv(operand),
+                _ => panic!("Invalid instruction!"),
+            };
+            if self.ptr == prev_ptr {
+                self.ptr += 2;
+            }
+        }
+        self.out.len() == self.instr.len()
+    }
+
+    fn is_out_still_same(&self) -> bool {
+        if self.out_cnt == 0 {
+            return true;
+        }
+        if self.out.len() > self.instr.len() {
+            return false;
+        }
+        (self.out[self.out_cnt - 1] as u8 - b'0') as usize == self.instr[self.out_cnt - 1]
+    }
+
+    fn combo(&self, operand: usize) -> usize {
+        match operand {
+            0..=3 => operand,
+            4 => self.reg_a,
+            5 => self.reg_b,
+            6 => self.reg_c,
+            _ => panic!("Invalid combo operand found!"),
         }
     }
 
@@ -99,6 +167,7 @@ impl Computer {
     fn out(&mut self, operand: usize) {
         self.out
             .push(((self.combo(operand) & 7) as u8 + b'0') as char);
+        self.out_cnt += 1;
     }
 
     fn bdv(&mut self, operand: usize) {
@@ -138,8 +207,15 @@ mod test {
 
     #[test]
     fn test_ex1() {
-        let input = input::read_example();
+        let input = input::read_file("input_example1.txt");
         let res = exercise1(&input);
         assert_eq!(res, "4,6,3,5,6,3,5,2,1,0");
+    }
+
+    #[test]
+    fn test_ex2() {
+        let input = input::read_file("input_example2.txt");
+        let res = exercise2(&input);
+        assert_eq!(res, 117440);
     }
 }
