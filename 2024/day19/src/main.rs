f8d55a1f11746ac7bdf65fc3a7493a7322768d21
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-12-19 08:51:14 +0100

    [19] feat: Solve exercise 2

--- 2024/day19/src/main.rs
+++ 2024/day19/src/main.rs
@@ -4,6 +4,7 @@ use utils::input;
 fn main() {
     let input = input::read_input();
     println!("exercise 1: {}", exercise1(&input));
+    println!("exercise 2: {}", exercise2(&input));
 }
 
 fn exercise1(input: &str) -> usize {
@@ -21,6 +22,25 @@ fn exercise1(input: &str) -> usize {
         // println!("selected: {}/{}", selected_available.len(), available.len(),);
         if is_possible(design, &selected_available, &mut cache) {
             possible += 1;
+        }
+    }
+    possible
+}
+
+fn exercise2(input: &str) -> usize {
+    let available: Vec<&str> = parse_available(input);
+    let wanted: Vec<&str> = parse_wanted(input);
+    let mut cache: HashMap<&str, Option<usize>> = HashMap::new();
+    let mut possible: usize = 0;
+
+    for design in wanted {
+        let selected_available: Vec<&str> = available
+            .iter()
+            .filter(|&a| design.contains(a))
+            .copied()
+            .collect();
+        if let Some(count) = count_possible(design, &selected_available, &mut cache) {
+            possible += count;
         }
     }
     possible
@@ -49,6 +69,34 @@ fn is_possible<'a>(
     false
 }
 
+fn count_possible<'a>(
+    wanted: &'a str,
+    available: &Vec<&str>,
+    cache: &mut HashMap<&'a str, Option<usize>>,
+) -> Option<usize> {
+    let mut res: Option<usize> = None;
+    let mut possible: usize = 0;
+
+    if let Some(&cached_res) = cache.get(wanted) {
+        return cached_res;
+    }
+    if wanted.is_empty() {
+        possible += 1;
+        return Some(possible);
+    }
+    for a in available {
+        if let Some(substr) = wanted.strip_prefix(a) {
+            if let Some(count) = count_possible(substr, available, cache) {
+                cache.insert(wanted, Some(count));
+                possible += count;
+                res = Some(possible);
+            }
+        }
+    }
+    cache.insert(wanted, res);
+    res
+}
+
 fn parse_available(input: &str) -> Vec<&str> {
     input.lines().nth(0).unwrap().split(", ").collect()
 }
@@ -66,5 +114,12 @@ mod test {
         let input = input::read_example();
         let res = exercise1(&input);
         assert_eq!(res, 6);
+    }
+
+    #[test]
+    fn test_ex2() {
+        let input = input::read_example();
+        let res = exercise2(&input);
+        assert_eq!(res, 16);
     }
 }
